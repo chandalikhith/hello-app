cadf349c6e5269b0c75caccf22eaac20ae3327b0
chandalikhith <chandalikhith79@gmail.com>
2026-04-13 08:51:39 +0530

    modifird

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,7 +1,14 @@
 package org.example;
 
 public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
     }
 }
