43cc3475829ba95585e13299eb426d718afb3f4c
chandalikhith <chandalikhith79@gmail.com>
2026-04-13 21:30:12 +0530

    Update UC6

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -7,14 +7,13 @@ public class HelloApp {
             System.out.println("Hello, World!");
         } else {
 
-            StringBuilder names = new StringBuilder();
+            StringBuilder nameBuilder = new StringBuilder();
 
             for (String name : args) {
-                if (names.length() > 0) {
-                    names.append(", ");
-                }
-                names.append(name);
+                nameBuilder.append(name).append(", ");
             }
+
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
 
             System.out.println("Hello, " + names + "!");
         }
