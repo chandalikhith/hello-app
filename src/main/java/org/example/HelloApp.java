a44c70dbe6d8ecb0cbd8373fdd3809770a7ed1e2
chandalikhith <chandalikhith79@gmail.com>
2026-04-13 09:20:34 +0530

    Revert "display hello with multiple args using substring to remove trailing delimiter"
    This reverts commit c4d39f8986809aa3204ef7af9cbaab590e62a587.

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -7,13 +7,14 @@ public class HelloApp {
             System.out.println("Hello, World!");
         } else {
 
-            StringBuilder nameBuilder = new StringBuilder();
+            StringBuilder names = new StringBuilder();
 
             for (String name : args) {
-                nameBuilder.append(name).append(", ");
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
             }
-
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
 
             System.out.println("Hello, " + names + "!");
         }
